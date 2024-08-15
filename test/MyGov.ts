654c71d862d3bf55e64257390a6de8c07bb31817
Sedi <sedmaster213@gmail.com>
2024-08-15 12:08:57 +0200

    change hre.ethers to ethers and move encodedFunctionCall and descriptionHash to deploy function

--- test/MyGov.ts
+++ test/MyGov.ts
@@ -1,14 +1,14 @@
 import { expect } from "chai";
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 import {
   loadFixture,
   time,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 async function moveBlocks(amount: number) {
-  const [owner, secondAccount] = await hre.ethers.getSigners();
+  const [owner, secondAccount] = await ethers.getSigners();
   for (let index = 0; index < amount; index++) {
-    // await hre.network.provider.send("emv_mine", []);
+    // await network.provider.send("emv_mine", []);
     //hardhat don`t allow to send emv_mine with auto mine block
     //so we just send tx
     await owner.sendTransaction({ to: secondAccount, value: 10 });
@@ -23,10 +23,10 @@ describe("MyGov", function () {
   const PROPOSAL_DESCRIPTION = "DoSomethingEXTRIMLYCool";
 
   async function deploy() {
-    const [owner, secondAccount, thirdAccount] = await hre.ethers.getSigners();
-    const MyGovFactory = await hre.ethers.getContractFactory("MyGov");
-    const MyTimeLockFactory = await hre.ethers.getContractFactory("MyTimeLock");
-    const myGovToken = await hre.ethers.deployContract("MyGovToken");
+    const [owner, secondAccount, thirdAccount] = await ethers.getSigners();
+    const MyGovFactory = await ethers.getContractFactory("MyGov");
+    const MyTimeLockFactory = await ethers.getContractFactory("MyTimeLock");
+    const myGovToken = await ethers.deployContract("MyGovToken");
 
     //immitate active user
     await myGovToken.mint(owner, OWNER_BALANCE);
@@ -48,10 +48,19 @@ describe("MyGov", function () {
       myGov.getAddress()
     );
 
+    const encodedFunctionCall = //@ts-ignore
+      myTimeLock.interface.encodeFunctionData(FUNC_TO_ENCODE);
+
+    const descriptionHash = ethers.keccak256(
+      ethers.toUtf8Bytes(PROPOSAL_DESCRIPTION)
+    );
+
     return {
       myGov,
       myTimeLock,
       myGovToken,
+      encodedFunctionCall,
+      descriptionHash,
       owner,
       secondAccount,
       thirdAccount,
@@ -63,13 +72,12 @@ describe("MyGov", function () {
       myGov,
       myTimeLock,
       myGovToken,
+      encodedFunctionCall,
+      descriptionHash,
       owner,
       secondAccount,
       thirdAccount,
     } = await loadFixture(deploy);
-
-    const encodedFunctionCall = //@ts-ignore
-      myTimeLock.interface.encodeFunctionData(FUNC_TO_ENCODE);
 
     await myGov.propose(
       [myTimeLock.getAddress()],
@@ -89,6 +97,8 @@ describe("MyGov", function () {
       myTimeLock,
       myGovToken,
       owner,
+      encodedFunctionCall,
+      descriptionHash,
       secondAccount,
       thirdAccount,
       proposalId,
@@ -105,12 +115,12 @@ describe("MyGov", function () {
       );
     });
   });
+
   describe("Proposal", function () {
     it("Should create proposal", async function () {
-      const { myGov, myTimeLock } = await loadFixture(deploy);
-
-      const encodedFunctionCall = //@ts-ignore
-        myTimeLock.interface.encodeFunctionData(FUNC_TO_ENCODE);
+      const { myGov, myTimeLock, encodedFunctionCall } = await loadFixture(
+        deploy
+      );
 
       await expect(
         myGov.propose(
@@ -135,12 +145,22 @@ describe("MyGov", function () {
         .withArgs(owner.address, proposalId, voteWay, OWNER_BALANCE, "");
     });
   });
+
   describe("Queue and Execute", function () {
     this.timeout(60000);
 
     it("Should queue and execute", async function () {
-      const { myGov, myTimeLock, secondAccount, proposalId } =
-        await loadFixture(deployWithPropalsasl);
+      this.timeout(60000); //because of moving block
+
+      const {
+        myGov,
+        myTimeLock,
+        secondAccount,
+        proposalId,
+        encodedFunctionCall,
+        descriptionHash,
+      } = await loadFixture(deployWithPropalsasl);
+
       const voteWay = 1;
       const BlocksAmountToAwait = 50401;
 
@@ -148,12 +168,6 @@ describe("MyGov", function () {
       await myGov.connect(secondAccount).castVote(proposalId, voteWay);
 
       await moveBlocks(BlocksAmountToAwait);
-
-      const encodedFunctionCall = //@ts-ignore
-        myTimeLock.interface.encodeFunctionData(FUNC_TO_ENCODE);
-      const descriptionHash = hre.ethers.keccak256(
-        hre.ethers.toUtf8Bytes(PROPOSAL_DESCRIPTION)
-      );
 
       await myGov.queue(
         [myTimeLock.getAddress()],
